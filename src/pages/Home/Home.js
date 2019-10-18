53c465c575b6b94e350af6e2128f53cf8053c27b
Emerson Paiva <emersonaraujopaiva@gmail.com>
2019-10-17 21:58:37 -0300

    Create Shop page and CollectionPreview component

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Directory from '../../components/Directory/Directory';
+
+import './Home.scss';
+
+const Home = () => (
+  <div className='homepage'>
+    <Directory />
+  </div>
+);
+
+export default Home;
--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-import HomePage from './pages/HomePage';
+import Home from './pages/Home/Home';
+import Shop from './pages/Shop/Shop';
 
 function Routes() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path='/' exact component={HomePage} />
+        <Route path='/' exact component={Home} />
+        <Route path='/shop' exact component={Shop} />
       </Switch>
     </BrowserRouter>
   );
