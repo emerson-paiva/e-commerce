4e5f73ecbc0dc264bbe8bd17c6a23012d7fe7b4b
Emerson Paiva <emersonaraujopaiva@gmail.com>
2019-11-17 20:45:40 -0300

    normalization shop data

--- src/pages/Collection/index.js
+++ src/pages/Collection/index.js
@@ -8,13 +8,19 @@ import { selectCollection } from '../../redux/shop/selectors';
 import './styles.scss';
 
 function Collection({ collection }) {
+  const { title, items } = collection;
+
   return (
     <div className='collection-page'>
-      <h2 className='title'>{collection.title}</h2>
+      <h2 className='title'>{title}</h2>
 
       <div className='items'>
-        {collection.items.map(item => (
-          <CollectionItem className='collection-item' item={item} />
+        {items.map(item => (
+          <CollectionItem
+            className='collection-item'
+            key={item.id}
+            item={item}
+          />
         ))}
       </div>
     </div>
